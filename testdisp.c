418c9e96b24b832615921daa6a0ab58f745fe44c
Moritz Barsnick <barsnick@gmx.net>
2023-01-01 17:55:26 +0100

    reindent code

--- testdisp.c
+++ testdisp.c
@@ -15,30 +15,30 @@ char *display;
 Display *hDisplay;
 
 int main(int argc, char **argv) {
-  if (argc > 2) {
-    fprintf( stderr, "%s: Too many arguments\n", argv[0] );
-    exit(2);
-  }
-  
-  if (argc == 2) {
-    display = argv[1];
-  }
-  else {
-    display = (char *)getenv("DISPLAY");
-  }
+    if (argc > 2) {
+        fprintf(stderr, "%s: Too many arguments\n", argv[0]);
+        exit(2);
+    }
 
-  if (!display) {
-    fprintf(stderr, "No display argument given and no DISPLAY environment variable set\n");
-    exit(2);
-  }
+    if (argc == 2) {
+        display = argv[1];
+    }
+    else {
+        display = (char *)getenv("DISPLAY");
+    }
 
-  hDisplay = XOpenDisplay(display);
-  if (!hDisplay) {
-    fprintf(stderr, "Could not open display '%s'\n", display);
-    exit(1);
-  }
-  else {
-    fprintf(stderr, "Successfully opened display '%s'\n", display);
-    exit(0);
-  }
+    if (!display) {
+        fprintf(stderr, "No display argument given and no DISPLAY environment variable set\n");
+        exit(2);
+    }
+
+    hDisplay = XOpenDisplay(display);
+    if (!hDisplay) {
+        fprintf(stderr, "Could not open display '%s'\n", display);
+        exit(1);
+    }
+    else {
+        fprintf(stderr, "Successfully opened display '%s'\n", display);
+        exit(0);
+    }
 }
